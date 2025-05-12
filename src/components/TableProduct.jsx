00bd7a0b3ce5e8de094686c6d6fd8b36b71eb5d7
Kevin Demetro <albinodemetrok@gmail.com>
2025-05-12 17:57:57 -0300

    Implement pagination, create example page and json dv

--- src/components/TableProduct.jsx
+++ src/components/TableProduct.jsx
@@ -1,10 +1,32 @@
+import { useEffect, useState } from "react";
 import Table from "./Table";
 import TableBody from "./TableBody";
 import TableCell from "./TableCell";
 import TableHeader from "./TableHeader";
+import TableFooter from "./TableFooter";
 import TableRow from "./TableRow";
 
-function TableProduct({ products }) {
+const PER_PAGE = 5;
+
+function TableProduct() {
+  const [products, setProducts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [numPages, setNumPages] = useState(1);
+  useEffect(
+    function () {
+      async function fetchProducts() {
+        const res = await fetch(
+          `http://localhost:3000/products?_page=${currentPage}&_per_page=${PER_PAGE}`
+        );
+        const data = await res.json();
+        setProducts(data.data);
+        setNumPages(data.pages);
+      }
+      fetchProducts();
+    },
+    [currentPage]
+  );
+
   return (
     <Table>
       <TableHeader>
@@ -21,6 +43,24 @@ function TableProduct({ products }) {
           </TableRow>
         ))}
       </TableBody>
+      <TableFooter>
+        <TableRow>
+          <TableCell>
+            <button
+              onClick={() => setCurrentPage((curr) => curr - 1)}
+              disabled={currentPage <= 1}
+            >
+              Prev
+            </button>
+            <button
+              onClick={() => setCurrentPage((curr) => curr + 1)}
+              disabled={currentPage >= numPages}
+            >
+              Next
+            </button>
+          </TableCell>
+        </TableRow>
+      </TableFooter>
     </Table>
   );
 }
---
+++ src/pages/Products.jsx
@@ -0,0 +1,12 @@
+import TableProduct from "../components/TableProduct";
+
+function Products() {
+  return (
+    <div style={{ padding: "50px" }}>
+      <h1>Products</h1>
+      <TableProduct />
+    </div>
+  );
+}
+
+export default Products;
