3a276ca12b43f09536d136de41b153375553544b
Kevin Demetro <albinodemetrok@gmail.com>
2025-05-13 07:25:30 -0300

    Start sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Products from "./pages/Products";
 
 function App() {
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          {/* {routes.map((route) => {
+            <Route to={route.path} element={route.element} />;
+          })} */}
+        </Routes>
+      </BrowserRouter>
+
       <Sidebar />
-      {/* <Products /> */}
     </>
   );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,10 @@
+import styles from "./Sidebar.module.css";
+function Sidebar() {
+  return (
+    <div className={`${styles.sidebar}`}>
+      <ul></ul>
+    </div>
+  );
+}
+
+export default Sidebar;
